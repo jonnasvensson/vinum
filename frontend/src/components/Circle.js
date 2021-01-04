87181300e258036113261372db4d7e0be423a0d4
Jonna Svensson <svensson.jonna@hotmail.com>
2021-01-04 17:52:05 +0100

    scss

--- frontend/src/components/Circle.js
+++ frontend/src/components/Circle.js
@@ -1,13 +1,17 @@
 import '../App.scss';
+import { Redirect, Link } from 'react-router-dom'
 
 export default function Circle() {
 
+
   return (
     <>
-      <div className="circle">
-        <div className="plusVerticle"></div>
-        <div className="plusHorizontal"></div>
-      </div>
+      <Link to="/add">
+        <div className="circle" >
+          <div className="plusVerticle"></div>
+          <div className="plusHorizontal"></div>
+        </div>
+      </Link>
     </>
   );
 }
